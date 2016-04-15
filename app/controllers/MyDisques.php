bbafde9f0e7b35e84a2062bd9b9897a39fd77af0
JoretGaetan <21500843@etu.unicaen.fr>
2016-04-15 17:55:57 +0200

    Quelques Tests

--- app/controllers/MyDisques.php
+++ app/controllers/MyDisques.php
@@ -2,12 +2,13 @@
 use micro\controllers\Controller;
 use micro\js\Jquery;
 use micro\utils\RequestUtils;
+
 class MyDisques extends Controller{
 	public function initialize(){
 		if(!RequestUtils::isAjax()){
 			$this->loadView("main/vHeader.html",array("infoUser"=>Auth::getInfoUser()));
             $user=Auth::getUser();
-            $this->loadView("main/vHeader.html",array("user"=>$user));
+            $this->loadView("main/vHeader.html",array("user"=>$user));  #VERIFICATION // LA VUE EST FAUSSE 
         }
 
 	}
