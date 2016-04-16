56147c5fea09e1d559546c43ab5da2cf5c99e8cb
JoretGaetan <21500843@etu.unicaen.fr>
2016-04-16 20:57:41 +0200

    Exemple de progressbar Il faut par la suite récupérer le quota des disques et configurer ce quota pour pouvoir afficher le taux d'occupation par disque

--- app/controllers/MyDisques.php
+++ app/controllers/MyDisques.php
@@ -6,13 +6,15 @@ use micro\utils\RequestUtils;
 class MyDisques extends Controller{
 	public function initialize(){
 		if(!RequestUtils::isAjax()){
-			$this->loadView("main/vHeader.html",array("infoUser"=>Auth::getInfoUser())); }
+			$this->loadView("main/vHeader.html",array("infoUser"=>Auth::getInfoUser()));
 
-		$user=Auth::getUser();
-		$disques=DAO::getOneToMany($user, "disques");
-		foreach($disques as $disque){
+			$user=Auth::getUser();
+			$disques=DAO::getOneToMany($user, "disques");
+			foreach($disques as $disque){
 				$this->loadview("MyDisques/disque.html",array("disques"=>$disques));
-		}
+			}
+
+        }
 
 	}
 	public function index() {
