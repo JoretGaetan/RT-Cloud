067e8276cc2ae8772675c163c167b9629ae0a427
JoretGaetan <21500843@etu.unicaen.fr>
2016-05-09 21:02:43 +0200

    TODO ADMIN ET CLIENT

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -3,7 +3,18 @@ use micro\orm\DAO;
 class Admin extends \BaseController {
 
 	public function index() {
+
 		$this->loadView("Administration/pageaccueil.html");
+
+	}
+
+	public function  users() {
+		$users=\micro\orm\DAO::getAll("Utilisateur");
+		$this->loadView("Administration/entete.html");
+		foreach ($users as $user) {
+			$nom =$user->getLogin();
+			$this->loadView("Administration/AccesDisk.html", array( "nom"=>$nom));
+		}
 
 	}
 }
